0400fcc62368d4404896b364c2bd3c3d1f043a81
Rubysdad <marktravismorales@gmail.com>
2019-08-13 15:25:20 -0500

    Configured session_store so app will work in development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,7 @@
-Rails.application.config.session_store :cookie_store, key: '_mulberry', domain: 'mulberry-api.herokuapp.com'
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store,
+                                         key: '_mulberry',
+                                         domain: 'mulberry-api.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_mulberry'
+end
